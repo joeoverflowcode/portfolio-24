66e8c6b1bfc95d64846e76848fc66f58c92bc393
Joe Aguado <aguado.joe@gmail.com>
2024-11-25 15:17:04 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Header from "./sections/Header"
+import Hero from "./sections/Hero"
+import Features from "./sections/Features"
+import Pricing from "./sections/Pricing"
+import Faq from "./sections/Faq"
+import Testimonials from "./sections/Testimonials"
+import Download from "./sections/Download"
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header/>
+      <Hero/>
+      <Features />
+      <Pricing />
+      <Faq />
+      <Testimonials />
+      <Download />
+    </main>
+  )
+}
+
+export default App
