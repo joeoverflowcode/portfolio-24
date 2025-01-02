0f3200b80695a49bf432193de658d79ea6cf48f9
Joe Aguado <aguado.joe@gmail.com>
2025-01-02 12:15:47 -0600

    about me updates

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -19,9 +19,16 @@ const About = () => {
             </div>
 
             <div className="z-20 flex flex-col container ">
-              <div className="relative min-w-[400px]">
+              <div className="relative min-w-[440px]">
               <h1 className="h3 md:h2">About Joe</h1>
-                <p className="body-1 mt-4 mb-10">Lorem ipsum odor amet, consectetuer adipiscing elit. Maximus habitasse aptent et, consectetur etiam ullamcorper ridiculus finibus euismod. Sollicitudin et lobortis fringilla semper ligula. Nulla odio a tortor vel feugiat mi diam congue. <br/><br/>Nibh fringilla scelerisque pharetra litora sociosqu dapibus. Litora proin nisl; elementum ex viverra proin. Nulla aptent consequat dignissim fermentum leo enim finibus. </p>
+                <p className="body-3 mt-4 mb-10">Hi, I’m Joe, I’m a web developer based out of Dallas, Texas. I’m passionate about finding ways that people and software can work together to make life better. I’m currently working on a few freelance projects for client’s in the DFW and NYC area.  
+                  <br /><br />
+                  I bring over 12+ years of professional experience working in Financial Services and Business Administration to the Technology sector. This experience has given me various skills but more importantly the capability to work individually or with teams in order to meet customer needs and company objectives.
+                  <br /><br />
+                  As a developer, I enjoy finding solutions that help bridge the gaps between business objectives and technology. I value user-friendly projects that try to solve real-world problems. I’m always looking for ways to give back to the various communities that have supported me over the years.  
+                  <br/><br/>
+                  Outside of development work, I find inspiritation spending time with my family, and being outdoors. My hunger and curiosity for life drive me to experience as much as I can with the time that is given to me. This ambition hopefully can be seen with the type of work and experiences I produce.
+                  </p>
                 <div className="flex gap-4 items-center lg:justify-center">
             <Download />
           <Contact />
